5688aec06a5634f7537df4d4f32176026d3b162e
TimoxGagarin <timarudkoooo@yandex.ru>
2021-11-15 11:59:08 +0300

    scroll buttons by jQuery

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ $(function () {
         $(this).addClass('active').siblings().removeClass('active');
     });
 	
+	$(".check-games").click(function(){
+		$("html, body").animate({scrollTop: $(".offer").height() + 400 },"slow");
+		return false;
+	});
+	
+	$(".to-start").click(function(){
+		$("html, body").animate({scrollTop: 0}, "slow");
+		return false;
+	});
 	
 });
